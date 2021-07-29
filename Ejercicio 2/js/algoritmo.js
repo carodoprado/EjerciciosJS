85ee1f7ab8d00f02eb72aa1a53a3d2a4b6cb26b5
carodoprado <dopradocarolina@gmail.com>
2021-07-28 22:52:49 -0300

    ejercicio 2

---
+++ Ejercicio 2/js/algoritmo.js
@@ -0,0 +1,14 @@
+var numero = prompt("Ingrese un número: ")
+numero = parseInt(numero);
+
+if (numero > 1000) {
+
+    alert("El número ingresado es mayor a 1000");
+} else if (numero == 1000) {
+
+    alert("El número ingresado es  1000");
+}
+else if (numero < 1000) {
+    alert("El número ingresado es menor a 1000");
+
+}
