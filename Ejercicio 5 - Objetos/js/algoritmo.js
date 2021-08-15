02bd678302cefdd24c9b74dc9c4a25b41b57c47b
carodoprado <dopradocarolina@gmail.com>
2021-08-14 21:51:08 -0300

    Ejercicio 5 - Objetos

---
+++ Ejercicio 5 - Objetos/js/algoritmo.js
@@ -0,0 +1,56 @@
+
+var resultado = 0;
+
+
+class Producto {
+      
+    constructor(nombre, valor){
+        this.nombre=nombre;
+        this.valor=valor;
+
+    }
+
+    getPrecio(){
+        return this.valor;
+    }
+} 
+
+do {
+
+    let nombre = pedirNombre();
+    let precio  = pedirValor();
+    
+    let producto=new Producto(nombre, precio);
+
+    console.log(producto);
+
+    resultado = sumaProducto(producto, resultado);
+
+} while (confirm("Desea ingresar un nuevo producto? "));
+
+
+
+alert("La suma total de productos es de: " + resultado);
+
+function pedirNombre() {
+
+    let nombre = prompt("Ingrese nombre del producto: ");
+    return nombre;
+}
+
+function pedirValor() {
+
+    let producto = parseFloat(prompt("Ingrese valor del producto: "));
+    return producto;
+}
+
+
+function sumaProducto(producto, resultado) {
+
+    
+    let total = resultado + producto.getPrecio();
+    return total;
+
+}
+
+
